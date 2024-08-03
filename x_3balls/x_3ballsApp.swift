ae9432b5083f1dbe6d2fb318c20698712c789197
fourcirclevenndiagram <unchainedsoul@nate.com>
2024-08-04 01:38:21 +0900

    Initial Commit

---
+++ x_3balls/x_3ballsApp.swift
@@ -0,0 +1,17 @@
+//
+//  x_3ballsApp.swift
+//  x_3balls
+//
+//  Created by Kim Min-Ki on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct x_3ballsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
