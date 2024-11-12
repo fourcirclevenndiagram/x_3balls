d0fe834d969e41ca8701a35cfece814474afe833
fourcirclevenndiagram <unchainedsoul@nate.com>
2024-11-12 19:11:04 +0900

    파라미터 설정

--- x_3balls/ContentView.swift
+++ x_3balls/ContentView.swift
@@ -30,7 +30,7 @@ struct ContentView: View {
                         offsetX = -20 // 다시 시작 위치로 초기화
                         
                         withAnimation(baseAnimation) {
-                            offsetX = 1950 // 다시 오른쪽으로 이동
+                            offsetX = 2000 // 다시 오른쪽으로 이동
                         }
                     }
                 }
