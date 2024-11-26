64de3900766a56146086ce6a887e7ec2160cf4ff
fourcirclevenndiagram <unchainedsoul@nate.com>
2024-11-26 19:19:50 +0900

    파라미터 설정

--- x_3balls/ContentView.swift
+++ x_3balls/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
                     let repeated = baseAnimation.repeatForever(autoreverses: false)
                     
                     withAnimation(repeated) {
-                        offsetX = 500 // 화면 오른쪽으로 통과하기 위한 임의의 값
+                        offsetX = 600 // 화면 오른쪽으로 통과하기 위한 임의의 값
                     }
                     
                     Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
